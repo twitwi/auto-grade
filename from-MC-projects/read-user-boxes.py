9de1c8e03a950537daab71fa6528d3dda7c2066a
Rémi Emonet <remi.emonet@reverse--com.heeere>
2017-06-24 21:21:48 +0200

    Save all images to disk

--- from-MC-projects/read-user-boxes.py
+++ from-MC-projects/read-user-boxes.py
@@ -1,5 +1,5 @@
 
-from scipy.misc import imread
+from scipy.misc import imread, imsave
 import io
 import sqlite3
 
@@ -14,10 +14,12 @@ def show_auto_grid(l):
     N = math.ceil(len(l)**0.5)
     M = math.ceil(len(l)/N)
     f, axarr = plt.subplots(M, N)
-    plt.axis('off')
     f.subplots_adjust(hspace=0)
     for i,a in enumerate(l):
         axarr[i//N, i%N].imshow(a, cmap='gray', interpolation='nearest')
+        axarr[i//N, i%N].axis('off')
+        imsave(",,test-"+str(i)+".jpg", a)
+    for i in range(len(l), N*M):
         axarr[i//N, i%N].axis('off')
     plt.show()
 
@@ -25,7 +27,7 @@ def show_auto_grid(l):
 conn = sqlite3.connect('capture.sqlite')
 c = conn.cursor()
 
-user = 2
+user = 1
 c.execute('''SELECT *
              FROM capture_zone
              WHERE student=? AND type=?
