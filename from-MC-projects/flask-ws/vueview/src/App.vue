b4b43db45a71ec745f45278231751eb7832aa7aa
Rémi Emonet <remi.emonet@reverse--com.heeere>
2018-06-01 00:20:27 +0200

    Refactor using vue-router + picnicss

--- from-MC-projects/flask-ws/vueview/src/App.vue
+++ from-MC-projects/flask-ws/vueview/src/App.vue
@@ -1,24 +1,37 @@
 <template>
   <div id="app">
-    <Test3/>
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+
+    <!-- picnicss navbar -->
+    <nav class="demo">
+      <router-link to="/">
+        <img class="logo" src="./assets/logo.png" />
+      </router-link>
+
+      <!-- responsive-->
+      <input id="bmenub" type="checkbox" class="show">
+      <label for="bmenub" class="burger pseudo button">menu</label>
+
+      <div class="menu">
+        <router-link to="/">Home</router-link>
+        <router-link to="/test2" class="pseudo button icon-picture">Raw</router-link>
+        <router-link to="/test3" class="pseudo button icon-picture">Annotate</router-link>
+        <!--a href="#" class="button icon-puzzle">Plugins</a-->
+      </div>
+    </nav>
+
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-import Test3 from './components/Test3'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld, Test3
-  }
+  name: 'App'
 }
 </script>
 
 <style>
+@import "../node_modules/picnic/picnic.min.css";
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -27,4 +40,5 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
 </style>
--- from-MC-projects/flask-ws/vueview/src/main.js
+++ from-MC-projects/flask-ws/vueview/src/main.js
@@ -1,17 +1,35 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import VueSocketio from 'vue-socket.io'
 import App from './App'
 import store from './store'
+// Components
+import HelloWorld from './components/HelloWorld'
+import Test3 from './components/Test3'
+import Test2 from './components/Test2'
+// could https://vuejs.org/v2/guide/components-registration.html#Automatic-Global-Registration-of-Base-Components
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
 Vue.use(VueSocketio, `//${window.document.domain}:5000`, store)
+
+let r = (path, component) => ({path, component})
+
+const router = new VueRouter({
+  routes: [
+    r('/', HelloWorld),
+    r('/test3', Test3),
+    r('/test2', Test2)
+  ]
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   components: { App },
+  router,
   store,
   template: '<App/>'
 })
